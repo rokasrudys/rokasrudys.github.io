ef5ccf550954091bc5d331daccd970435265dbe7
rokas <rokasrudys@outlook.com>
2022-02-18 15:20:07 +0000

    Deploy Next.js to gh-pages

---
+++ _next/static/TDki-D5srN_mbHKxoFt5Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-26e2051140102cd6.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
